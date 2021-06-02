7f7d30d8f2f2b1306f48464f982a4a7cd94fac20
Asadullah-Dal17 <aiphile17@gmail.com>
2021-06-02 12:36:30 +0500

    updating distance finder yolo with yolo object detector

--- Yolo_distance/yolov4.py
+++ Yolo_distance/yolov4.py
@@ -1,5 +1,11 @@
+
 import cv2 as cv
 import time
+# Required Distance data
+MobileWith = 3.0  # inches
+PersonWidth = 16  # Inches
+KnownDistance = 45  # Inches
+
 # setting parameters
 CONFIDENCE_THRESHOLD = 0.5
 NMS_THRESHOLD = 0.5
@@ -33,16 +39,29 @@ yoloNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
 model = cv.dnn_DetectionModel(yoloNet)
 model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
-# setting camera
+
+def FocalLength(measured_distance, real_width, width_in_rf_image):
+
+    focal_length = (width_in_rf_image * measured_distance) / real_width
+    return focal_length
+# distance estimation function
 
 
-def ObjectDetector(image):
+def Distance_finder(Focal_Length, real_object_width, object_width_in_frame):
+
+    distance = (real_object_width * Focal_Length)/object_width_in_frame
+    return distance
+
+
+def ObjectDetector(image,  id):
     classes, scores, boxes = model.detect(
         image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
     for (classid, score, box) in zip(classes, scores, boxes):
         color = COLORS[int(classid) % len(COLORS)]
         label = "%s : %f" % (class_names[classid[0]], score)
+        if id == classid:
+            print('detected')
         cv.rectangle(image, box, color, 2)
         cv.putText(frame, label, (box[0], box[1]-10), fonts, 0.5, color, 2)
 
@@ -52,7 +71,7 @@ camera = cv.VideoCapture(0)
 while True:
     ret, frame = camera.read()
     orignal = frame.copy()
-    ObjectDetector(frame)
+    ObjectDetector(frame, 67)
     cv.imshow('oringal', orignal)
     cv.imshow('frame', frame)
 
