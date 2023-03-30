222ad396d4216930e06d2a15bff1d4eb033a86b3
rosenpin <mail@tomerrosenfeld.com>
2023-03-30 23:13:41 +0300

    notify server with request

--- start_camera.py
+++ start_camera.py
@@ -1,5 +1,7 @@
 import sys
 
+import requests
+
 from Speed.updated_speed import *
 
 
@@ -11,24 +13,48 @@ def main():
         # print(f"distance: {distance} , speed: {speed}")
         if should_alert(distance, speed):
             alert(distance, speed, frame)
+        else:
+            relax(calculate_danger(distance, speed))
 
 
 def send_frame(frame):
     pass
 
 
-def notify_server(distance, speed):
-    pass
+def notify_server(danger, alert=1):
+    try:
+        params = {'danger': danger, 'alert': alert}
+        response = requests.post('http://localhost:3001/notify', params=params)
+        print(response)
+    except Exception as e:
+        pass
+
+
+def calculate_danger(distance, speed):
+    danger = 15 / distance + 4 * speed
+    return danger
+
+
+relax_counter = 0
+
+
+def relax(danger):
+    global relax_counter
+    print("relaxing")
+    relax_counter += 1
+    if relax_counter >= 10:
+        notify_server(danger=danger, alert=0)
+        relax_counter = 0
 
 
 def alert(distance, speed, frame):
     send_frame(frame)
-    notify_server(distance, speed)
+    notify_server(danger=calculate_danger(distance, speed))
     print(f"ALERT: distance: {distance} , speed: {speed}")
 
 
 def should_alert(distance, speed):
-    return distance < 2 and speed > 3 or distance < 0.5
+    return (speed >= 0 or distance < 0.5) and calculate_danger(distance, speed) > 30
 
 
 if __name__ == '__main__':
