9d3c93fc2ccb1c3140792478a95299e356b4f261
rosenpin <mail@tomerrosenfeld.com>
2023-03-30 22:12:40 +0300

    alert option

--- Speed/updated_speed.py
+++ Speed/updated_speed.py
@@ -1,250 +1,238 @@
-import sys
 import time
 
 import cv2
 
-gui = sys.argv[1] == 'gui'
 
-# variables
-# distance from camera to object(face) measured
-Known_distance = 30  # Inches
-# mine is 14.3 something, measure your face width, are google it
-Known_width = 5.7  # Inches
+def get_incoming_danger(gui):
+    # variables
+    # distance from camera to object(face) measured
+    Known_distance = 30  # Inches
+    # mine is 14.3 something, measure your face width, are google it
+    Known_width = 5.7  # Inches
 
-# Colors  >>> BGR Format(BLUE, GREEN, RED)
-GREEN = (0, 255, 0)
-RED = (0, 0, 255)
-BLACK = (0, 0, 0)
-YELLOW = (0, 255, 255)
-WHITE = (255, 255, 255)
-CYAN = (255, 255, 0)
-MAGENTA = (255, 0, 242)
-GOLDEN = (32, 218, 165)
-LIGHT_BLUE = (255, 9, 2)
-PURPLE = (128, 0, 128)
-CHOCOLATE = (30, 105, 210)
-PINK = (147, 20, 255)
-ORANGE = (0, 69, 255)
+    # Colors  >>> BGR Format(BLUE, GREEN, RED)
+    GREEN = (0, 255, 0)
+    RED = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    YELLOW = (0, 255, 255)
+    WHITE = (255, 255, 255)
+    CYAN = (255, 255, 0)
+    MAGENTA = (255, 0, 242)
+    GOLDEN = (32, 218, 165)
+    LIGHT_BLUE = (255, 9, 2)
+    PURPLE = (128, 0, 128)
+    CHOCOLATE = (30, 105, 210)
+    PINK = (147, 20, 255)
+    ORANGE = (0, 69, 255)
 
-fonts = cv2.FONT_HERSHEY_COMPLEX
-fonts2 = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-fonts3 = cv2.FONT_HERSHEY_COMPLEX_SMALL
-fonts4 = cv2.FONT_HERSHEY_TRIPLEX
-# Camera Object
-capID = 0
-cap = cv2.VideoCapture(capID)  # Number According to your Camera
-Distance_level = 0
-travedDistance = 0
-changeDistance = 0
-velocity = 0
+    fonts = cv2.FONT_HERSHEY_COMPLEX
+    fonts2 = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+    fonts3 = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    fonts4 = cv2.FONT_HERSHEY_TRIPLEX
+    # Camera Object
+    capID = 0
+    cap = cv2.VideoCapture(capID)  # Number According to your Camera
+    Distance_level = 0
+    travedDistance = 0
+    changeDistance = 0
+    velocity = 0
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output21.mp4', fourcc, 30.0, (640, 480))
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output21.mp4', fourcc, 30.0, (640, 480))
 
-# face detector object
-face_detector = cv2.CascadeClassifier("../haarcascade_frontalface_default.xml")
+    # face detector object
+    face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+    # focal length finder function
 
-# focal length finder function
+    def FocalLength(measured_distance, real_width, width_in_rf_image):
+        # Function Discrption (Doc String)
+        '''
+        This Function Calculate the Focal Length(distance between lens to CMOS sensor), it is simple constant we can find by using
+        MEASURED_DISTACE, REAL_WIDTH(Actual width of object) and WIDTH_OF_OBJECT_IN_IMAGE
+        :param1 Measure_Distance(int): It is distance measured from object to the Camera while Capturing Reference image
 
+        :param2 Real_Width(int): It is Actual width of object, in real world (like My face width is = 5.7 Inches)
+        :param3 Width_In_Image(int): It is object width in the frame /image in our case in the reference image(found by Face detector)
+        :retrun Focal_Length(Float):
+        '''
+        focal_length = (width_in_rf_image * measured_distance) / real_width
+        return focal_length
 
-def FocalLength(measured_distance, real_width, width_in_rf_image):
-    # Function Discrption (Doc String)
-    '''
-    This Function Calculate the Focal Length(distance between lens to CMOS sensor), it is simple constant we can find by using 
-    MEASURED_DISTACE, REAL_WIDTH(Actual width of object) and WIDTH_OF_OBJECT_IN_IMAGE 
-    :param1 Measure_Distance(int): It is distance measured from object to the Camera while Capturing Reference image
+    # distance estimation function
 
-    :param2 Real_Width(int): It is Actual width of object, in real world (like My face width is = 5.7 Inches)
-    :param3 Width_In_Image(int): It is object width in the frame /image in our case in the reference image(found by Face detector) 
-    :retrun Focal_Length(Float):
-    '''
-    focal_length = (width_in_rf_image * measured_distance) / real_width
-    return focal_length
+    def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
+        '''
+        This Function simply Estimates the distance between object and camera using arguments(Focal_Length, Actual_object_width, Object_width_in_the_image)
+        :param1 Focal_length(float): return by the Focal_Length_Finder function
 
+        :param2 Real_Width(int): It is Actual width of object, in real world (like My face width is = 5.7 Inches)
+        :param3 object_Width_Frame(int): width of object in the image(frame in our case, using Video feed)
+        :return Distance(float) : distance Estimated
 
-# distance estimation function
+        '''
+        distance = (real_face_width * Focal_Length) / face_width_in_frame
+        return distance
 
+    def speedFinder(distance, takenTime):
+        speed = distance / takenTime
+        return speed
 
-def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
-    '''
-    This Function simply Estimates the distance between object and camera using arguments(Focal_Length, Actual_object_width, Object_width_in_the_image)
-    :param1 Focal_length(float): return by the Focal_Length_Finder function
+    # face detection Fauction
 
-    :param2 Real_Width(int): It is Actual width of object, in real world (like My face width is = 5.7 Inches)
-    :param3 object_Width_Frame(int): width of object in the image(frame in our case, using Video feed)
-    :return Distance(float) : distance Estimated  
+    def face_data(image, CallOut, Distance_level):
+        '''
 
-    '''
-    distance = (real_face_width * Focal_Length) / face_width_in_frame
-    return distance
+        This function Detect face and Draw Rectangle and display the distance over Screen
 
+        :param1 Image(Mat): simply the frame
+        :param2 Call_Out(bool): If want show Distance and Rectangle on the Screen or not
+        :param3 Distance_Level(int): which change the line according the Distance changes(Intractivate)
+        :return1  face_width(int): it is width of face in the frame which allow us to calculate the distance and find focal length
+        :return2 face(list): length of face and (face paramters)
+        :return3 face_center_x: face centroid_x coordinate(x)
+        :return4 face_center_y: face centroid_y coordinate(y)
 
-def speedFinder(distance, takenTime):
-    speed = distance / takenTime
-    return speed
+        '''
 
+        face_width = 0
+        face_x, face_y = 0, 0
+        face_center_x = 0
+        face_center_y = 0
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
 
-# face detection Fauction
+        faces_with_id = []
+        # add id to each face in the faces array
+        for index, face in enumerate(faces):
+            x, y, w, h = face
+            faces_with_id.append([x, y, w, h, index + 1])
 
-def face_data(image, CallOut, Distance_level):
-    '''
+        faces = faces_with_id
 
-    This function Detect face and Draw Rectangle and display the distance over Screen
+        for (x, y, h, w, index) in faces:
 
-    :param1 Image(Mat): simply the frame 
-    :param2 Call_Out(bool): If want show Distance and Rectangle on the Screen or not
-    :param3 Distance_Level(int): which change the line according the Distance changes(Intractivate)
-    :return1  face_width(int): it is width of face in the frame which allow us to calculate the distance and find focal length
-    :return2 face(list): length of face and (face paramters)
-    :return3 face_center_x: face centroid_x coordinate(x)
-    :return4 face_center_y: face centroid_y coordinate(y)
+            line_thickness = 2
+            # print(len(faces))
+            LLV = int(h * 0.12)
+            # print(LLV)
 
-    '''
+            # cv2.rectangle(image, (x, y), (x+w, y+h), BLACK, 1)
+            cv2.line(image, (x, y + LLV), (x + w, y + LLV), (GREEN), line_thickness)
+            cv2.line(image, (x, y + h), (x + w, y + h), (GREEN), line_thickness)
+            cv2.line(image, (x, y + LLV), (x, y + LLV + LLV), (GREEN), line_thickness)
+            cv2.line(image, (x + w, y + LLV), (x + w, y + LLV + LLV),
+                     (GREEN), line_thickness)
+            cv2.line(image, (x, y + h), (x, y + h - LLV), (GREEN), line_thickness)
+            cv2.line(image, (x + w, y + h), (x + w, y + h - LLV), (GREEN), line_thickness)
 
-    face_width = 0
-    face_x, face_y = 0, 0
-    face_center_x = 0
-    face_center_y = 0
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
+            face_width = w
+            face_center = []
+            # Drwaing circle at the center of the face
+            face_center_x = int(w / 2) + x
+            face_center_y = int(h / 2) + y
+            if Distance_level < 10:
+                Distance_level = 10
 
-    faces_with_id = []
-    # add id to each face in the faces array
-    for index, face in enumerate(faces):
-        x, y, w, h = face
-        faces_with_id.append([x, y, w, h, index + 1])
+            # cv2.circle(image, (face_center_x, face_center_y),5, (255,0,255), 3 )
+            if CallOut == True:
+                # cv2.line(image, (x,y), (face_center_x,face_center_y ), (155,155,155),1)
+                cv2.line(image, (x, y - 11), (x + 180, y - 11), (ORANGE), 28)
+                cv2.line(image, (x, y - 11), (x + 180, y - 11), (YELLOW), 20)
+                cv2.line(image, (x, y - 11), (x + Distance_level, y - 11), (GREEN), 18)
 
-    faces = faces_with_id
+                # cv2.circle(image, (face_center_x, face_center_y),2, (255,0,255), 1 )
+                # cv2.circle(image, (x, y),2, (255,0,255), 1 )
 
-    for (x, y, h, w, index) in faces:
+            # face_x = x
+            # face_y = y
+            break
 
-        line_thickness = 2
-        # print(len(faces))
-        LLV = int(h * 0.12)
-        # print(LLV)
+        return face_width, faces, face_center_x, face_center_y
 
-        # cv2.rectangle(image, (x, y), (x+w, y+h), BLACK, 1)
-        cv2.line(image, (x, y + LLV), (x + w, y + LLV), (GREEN), line_thickness)
-        cv2.line(image, (x, y + h), (x + w, y + h), (GREEN), line_thickness)
-        cv2.line(image, (x, y + LLV), (x, y + LLV + LLV), (GREEN), line_thickness)
-        cv2.line(image, (x + w, y + LLV), (x + w, y + LLV + LLV),
-                 (GREEN), line_thickness)
-        cv2.line(image, (x, y + h), (x, y + h - LLV), (GREEN), line_thickness)
-        cv2.line(image, (x + w, y + h), (x + w, y + h - LLV), (GREEN), line_thickness)
+    def averageFinder(valuesList, numberElements):
+        sizeOfList = len(valuesList)
+        lastMostElement = sizeOfList - numberElements
+        lastPart = valuesList[lastMostElement:]
+        average = sum(lastPart) / (len(lastPart))
+        return average
 
-        face_width = w
-        face_center = []
-        # Drwaing circle at the center of the face
-        face_center_x = int(w / 2) + x
-        face_center_y = int(h / 2) + y
-        if Distance_level < 10:
-            Distance_level = 10
+    # reading reference image from directory
+    ref_image = cv2.imread("Ref_image.png")
 
-        # cv2.circle(image, (face_center_x, face_center_y),5, (255,0,255), 3 )
-        if CallOut == True:
-            # cv2.line(image, (x,y), (face_center_x,face_center_y ), (155,155,155),1)
-            cv2.line(image, (x, y - 11), (x + 180, y - 11), (ORANGE), 28)
-            cv2.line(image, (x, y - 11), (x + 180, y - 11), (YELLOW), 20)
-            cv2.line(image, (x, y - 11), (x + Distance_level, y - 11), (GREEN), 18)
+    ref_image_face_width, _, _, _ = face_data(ref_image, False, Distance_level)
+    Focal_length_found = FocalLength(
+        Known_distance, Known_width, ref_image_face_width)
+    print(Focal_length_found)
 
-            # cv2.circle(image, (face_center_x, face_center_y),2, (255,0,255), 1 )
-            # cv2.circle(image, (x, y),2, (255,0,255), 1 )
+    speedMap = {}
+    distanceMap = {}
+    averageSpeed = {}
+    initialDistance = {}
+    while True:
+        _, frame = cap.read()
+        # calling face_data function
+        # Distance_leve =0
+        intialTime = time.time()
+        face_width_in_frame, Faces, FC_X, FC_Y = face_data(
+            frame, True, Distance_level)
+        distances = {}
+        # finding the distance by calling function Distance finder
+        for (face_x, face_y, face_w, face_h, face_id) in Faces:
+            Distance = Distance_finder(Focal_length_found, Known_width, face_width_in_frame)
+            if face_id not in distanceMap or distanceMap[face_id] is None:
+                distanceMap[face_id] = []
 
-        # face_x = x
-        # face_y = y
-        break
+            distanceMap[face_id].append(Distance)
+            if len(distanceMap[face_id]) > 6:
+                distanceMap[face_id].pop(0)
+            avergDistnce = averageFinder(distanceMap[face_id], 6)
+            # print(avergDistnce)
+            roundedDistance = round((avergDistnce * 0.0254), 2)
+            # Drwaing Text on the screen
+            Distance_level = int(Distance)
+            if face_id in initialDistance and initialDistance[face_id] != 0:
+                changeDistance = initialDistance[face_id] - Distance
+                distanceInMeters = changeDistance * 0.0254
 
-    return face_width, faces, face_center_x, face_center_y
+                velocity = speedFinder(distanceInMeters, changeInTime)
 
+                if face_id not in speedMap:
+                    speedMap[face_id] = []
+                speedMap[face_id].append(velocity)
 
-def averageFinder(valuesList, numberElements):
-    sizeOfList = len(valuesList)
-    lastMostElement = sizeOfList - numberElements
-    lastPart = valuesList[lastMostElement:]
-    average = sum(lastPart) / (len(lastPart))
-    return average
+                averageSpeed[face_id] = averageFinder(speedMap[face_id], 6)
+            # intial Distance
+            initialDistance[face_id] = avergDistnce
 
+            changeInTime = time.time() - intialTime
+            # print(changeInTime)
 
-# reading reference image from directory
-ref_image = cv2.imread("../Ref_image.png")
+            cv2.line(frame, (25, face_id * 45), (180, face_id * 45), (ORANGE), 26)
+            cv2.line(frame, (25, face_id * 45), (180, face_id * 45), (GREEN), 20)
+            # cv2.line(image, (x, y-11), (x+180, y-11), (YELLOW), 20)
+            # cv2.line(image, (x, y-11), (x+Distance_level, y-11), (GREEN), 18)
+            if face_id not in averageSpeed:
+                averageSpeed[face_id] = 0
+            cv2.putText(
+                frame, f"{face_id} Speed: {round(averageSpeed[face_id], 2)} m/s", (30, 50 * face_id), fonts, 0.5, BLACK,
+                2)
 
-ref_image_face_width, _, _, _ = face_data(ref_image, False, Distance_level)
-Focal_length_found = FocalLength(
-    Known_distance, Known_width, ref_image_face_width)
-print(Focal_length_found)
+            distances[face_id] = roundedDistance
+            cv2.putText(frame, f"{face_id}  Distance {roundedDistance} meter",
+                        (face_x - 6, face_y - 6), fonts, 0.5, (BLACK), 2)
 
+            if face_id in speedMap and face_id in distances:
+                yield distances[face_id], averageSpeed[face_id], frame
+            break
+        if gui:
+            cv2.imshow("frame", frame)
+        out.write(frame)
 
-if gui:
-    cv2.imshow("ref_image", ref_image)
-speedMap = {}
-distanceMap = {}
-averageSpeed = {}
-initialDistance = {}
+        if cv2.waitKey(1) == ord("q"):
+            break
 
-while True:
-    _, frame = cap.read()
-    # calling face_data function
-    # Distance_leve =0
-    intialTime = time.time()
-    face_width_in_frame, Faces, FC_X, FC_Y = face_data(
-        frame, True, Distance_level)
-    distances = {}
-    # finding the distance by calling function Distance finder
-    for (face_x, face_y, face_w, face_h, face_id) in Faces:
-        Distance = Distance_finder(Focal_length_found, Known_width, face_width_in_frame)
-        if face_id not in distanceMap or distanceMap[face_id] is None:
-            distanceMap[face_id] = []
-
-        distanceMap[face_id].append(Distance)
-        if len(distanceMap[face_id]) > 6:
-            distanceMap[face_id].pop(0)
-        avergDistnce = averageFinder(distanceMap[face_id], 6)
-        # print(avergDistnce)
-        roundedDistance = round((avergDistnce * 0.0254), 2)
-        # Drwaing Text on the screen
-        Distance_level = int(Distance)
-        if face_id in initialDistance and initialDistance[face_id] != 0:
-            changeDistance = initialDistance[face_id] - Distance
-            distanceInMeters = changeDistance * 0.0254
-
-            velocity = speedFinder(distanceInMeters, changeInTime)
-
-            if face_id not in speedMap:
-                speedMap[face_id] = []
-            speedMap[face_id].append(velocity)
-
-            averageSpeed[face_id] = averageFinder(speedMap[face_id], 6)
-        # intial Distance
-        initialDistance[face_id] = avergDistnce
-
-        changeInTime = time.time() - intialTime
-        # print(changeInTime)
-
-        cv2.line(frame, (25, face_id * 45), (180, face_id * 45), (ORANGE), 26)
-        cv2.line(frame, (25, face_id * 45), (180, face_id * 45), (GREEN), 20)
-        # cv2.line(image, (x, y-11), (x+180, y-11), (YELLOW), 20)
-        # cv2.line(image, (x, y-11), (x+Distance_level, y-11), (GREEN), 18)
-        if face_id not in averageSpeed:
-            averageSpeed[face_id] = 0
-        cv2.putText(
-            frame, f"{face_id} Speed: {round(averageSpeed[face_id], 2)} m/s", (30, 50 * face_id), fonts, 0.5, BLACK, 2)
-
-        distances[face_id] = roundedDistance
-        cv2.putText(frame, f"{face_id}  Distance {roundedDistance} meter",
-                    (face_x - 6, face_y - 6), fonts, 0.5, (BLACK), 2)
-
-        break
-    if gui:
-        cv2.imshow("frame", frame)
-    out.write(frame)
-    print(distanceMap)
-    print(distances)
-
-    if cv2.waitKey(1) == ord("q"):
-        break
-
-cap.release()
-# out.release()
-cv2.destroyAllWindows()
+    cap.release()
+    # out.release()
+    cv2.destroyAllWindows()
---
+++ start_camera.py
@@ -0,0 +1,24 @@
+import sys
+
+from Speed.updated_speed import *
+
+
+def main():
+    gui = False
+    if len(sys.argv) >= 2:
+        gui = sys.argv[1] == 'gui'
+    for (distance, speed, frame) in get_incoming_danger(gui):
+        # print(f"distance: {distance} , speed: {speed}")
+        if should_alert(distance, speed):
+            alert(distance, speed)
+
+
+def alert(distance, speed):
+    print(f"ALERT: distance: {distance} , speed: {speed}")
+
+def should_alert(distance, speed):
+    return distance < 2 and speed > 3 or distance < 0.5
+
+
+if __name__ == '__main__':
+    main()
